06113d71354872b63cd70fff95e3e8a8c71015e4
Mac <mac@Macde-MacBook-Pro.local>
2020-04-12 10:24:57 +0800

    add header

--- product.py
+++ product.py
@@ -8,6 +8,7 @@ while True :
 	if name == 'q' :
 		break
 	price = input('商品價格 : ')
+	price = int(price)
 	product.append([name , price])
 
 print(product)
@@ -17,4 +18,13 @@ for p in product :
 	print(p[0] , '價格是' , p[1])
 
 print('總共', len(product) ,'幾筆')
+
+with open('product.csv' , 'w' , encoding = 'utf-8') as f :
+	f.write('商品,價格\n')
+	for p in product :
+		f.write(p[0] + ',' + str(p[1]) + '\n')
+
+
+
+
 
